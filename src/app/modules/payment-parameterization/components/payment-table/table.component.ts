66b649539d90defc988644d27d878dbfb59a5d11
Samuel Suarez <samisua16@gmail.com>
2025-06-24 22:38:11 -0500

    Implement alert and payment forms with validation; enhance table components for displaying alerts and payments

--- src/app/modules/payment-parameterization/components/payment-table/table.component.ts
+++ src/app/modules/payment-parameterization/components/payment-table/table.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-payment-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class PaymentTableComponent {
-
+  displayedColumns: string[] = ['name', 'amount'];
+  
+  dataSource = [
+    { id: 1, name: 'Pago 1', amount: 100, status: 'Completado' },
+    { id: 2, name: 'Pago 2', amount: 250, status: 'Pendiente' },
+    { id: 3, name: 'Pago 3', amount: 75, status: 'Fallido' }
+  ];
 }
