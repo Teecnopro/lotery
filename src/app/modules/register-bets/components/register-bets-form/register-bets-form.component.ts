b4eefbf53d4c05fcb4616f5b917bdd2a6fd8a242
MonkeyStriker <jesusdeveloper404@gmail.com>
2025-06-25 23:06:25 -0500

    Estilos iniciales

--- src/app/modules/register-bets/components/register-bets-form/register-bets-form.component.ts
+++ src/app/modules/register-bets/components/register-bets-form/register-bets-form.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelect, MatSelectModule } from '@angular/material/select';
+import { MatOptionModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { lotteries, sellers } from '../../mocks/mocks';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-register-bets-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+    NgFor,
+    MatCheckboxModule
+  ],
   templateUrl: './register-bets-form.component.html',
-  styleUrl: './register-bets-form.component.scss'
+  styleUrl: './register-bets-form.component.scss',
+  providers: [provideNativeDateAdapter()]
 })
 export class RegisterBetsFormComponent {
+   @ViewChild('lotterySelect') lotterySelect!: MatSelect;
 
+  private formBuilder = inject(FormBuilder);
+
+  registerBetForm: FormGroup = this.formBuilder.group({
+    date: [new Date(), [Validators.required]],
+    lottery: [{value: "", disabled: true}, [Validators.required], ],
+    seller: [{value: ""}, [Validators.required], ],
+    lotteryNumber: ["", [Validators.required, Validators.minLength(1), Validators.maxLength(5)], ],
+    combined: [false, [Validators.required] ],
+    value: [0, [Validators.required, Validators.min(1)]]
+  });
+
+  arrayLotteries = lotteries;
+  arraySellers = sellers;
+
+  constructor() {}
+
+  activeDeactiveSelect(action: "active" | "deactive"): void {
+    const lottery = this.registerBetForm.get("lottery");
+
+    action === "active" ? (lottery?.enable(), this.lotterySelect.open()) : lottery?.disable();
+  }
 }
--- src/app/modules/register-bets/components/register-bets-resume/register-bets-resume.component.ts
+++ src/app/modules/register-bets/components/register-bets-resume/register-bets-resume.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-register-bets-resume',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './register-bets-resume.component.html',
-  styleUrl: './register-bets-resume.component.scss'
+  styleUrl: './register-bets-resume.component.scss',
 })
-export class RegisterBetsResumeComponent {
-
-}
+export class RegisterBetsResumeComponent {}
