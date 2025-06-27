cde0b13b196a343e4b1579962af616ef7de3da2d
Samuel Suarez <samisua16@gmail.com>
2025-06-26 21:23:22 -0500

    Refactor payment parameterization use case methods to use specific data types

--- src/app/domain/payment-parameterization/use-cases/payment-parameterization.usecases.ts
+++ src/app/domain/payment-parameterization/use-cases/payment-parameterization.usecases.ts
@@ -1,23 +1,23 @@
 import { inject } from '@angular/core';
 
 import { PAYMENT_PARAMETERIZATION_SERVICE } from '../ports';
+import { PaymentParameterization } from '../models/payment-parameterization.entity';
 
 export class PaymentParameterizationUseCase {
   private paymentParameterization = inject(PAYMENT_PARAMETERIZATION_SERVICE);
-  createPaymentParameterization(data: any): Promise<any> {
+  createPaymentParameterization(data: PaymentParameterization): Promise<any> {
     return this.paymentParameterization.createPaymentParameterization(data);
   }
-  updatePaymentParameterization(id: string, data: any): Promise<any> {
+  updatePaymentParameterization(id: string, data: PaymentParameterization): Promise<any> {
     return this.paymentParameterization.updatePaymentParameterization(id, data);
   }
   deletePaymentParameterization(id: string): Promise<void> {
     return this.paymentParameterization.deletePaymentParameterization(id);
   }
-  getPaymentParameterization(id: string): Promise<any | null> {
+  getPaymentParameterization(id: string): Promise<PaymentParameterization | null> {
     return this.paymentParameterization.getPaymentParameterization(id);
   }
-  listPaymentParameterizations(): Promise<any[]> {
+  listPaymentParameterizations(): Promise<PaymentParameterization[]> {
     return this.paymentParameterization.listPaymentParameterizations();
   }
-  
 }
