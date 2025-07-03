45b2290abdeea30d23d7b83f5ac0047a34ea9a6c
Samuel Suarez <samisua16@gmail.com>
2025-07-02 20:31:08 -0500

    Fix seller object structure in register bets form submission

--- src/app/modules/register-bets/components/register-bets-form/register-bets-form.component.ts
+++ src/app/modules/register-bets/components/register-bets-form/register-bets-form.component.ts
@@ -131,7 +131,7 @@ export class RegisterBetsFormComponent implements OnInit {
       lottery: { id: form.lottery._id, name: form.lottery.name },
       date: Timestamp.fromDate(date),
       lotteryNumber: form.lotteryNumber,
-      seller: { id: form.seller._id, name: form.seller.seller },
+      seller: { id: form.seller.uid, name: form.seller.name },
       combined: form.combined,
       value: form.value,
       createdAt: Timestamp.now(),
