4c68b73dd5a9f7196fbbee1cc15d7afc89a6067a
Samuel Suarez <samisua16@gmail.com>
2025-07-04 22:39:00 -0500

    Implement Logbook form and table components with filtering functionality, including date range and user selection, and enhance table display with pagination and loading states.

--- src/app/modules/logbook/component/logbook-form/form.component.ts
+++ src/app/modules/logbook/component/logbook-form/form.component.ts
@@ -1,17 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../../../shared/material/material.module';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AuthUser } from '../../../../domain/auth/models/auth-user.entity';
 
 @Component({
     selector: 'app-logbook-form',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.scss'],
+    imports: [
+        CommonModule,
+        FormsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatCardModule,
+    ],
     standalone: true
 })
 export class LogbookFormComponent implements OnInit {
+    textButton: string = 'Filtrar';
+    logBookFilter = {};
+    today: Date = new Date();
+    users: AuthUser[] = []
+    selectedUser: AuthUser | null = null;
+    selectedActionType: string | null = null;
+    selectedModule: string | null = null;
+    actionTypes: string[] = ['Creación', 'Actualización', 'Eliminación'];
+    modules: string[] = ['Usuarios', 'Alertas', 'Pagos', 'Apuestas', 'Vendedores', 'Reportes', 'Bitácora'];
+    dateRange: { start: Date | null, end: Date | null } = { start: null, end: null };
+    dateRangeStart: Date | null = null;
+    dateRangeEnd: Date | null = null;
 
     constructor() { }
 
     ngOnInit(): void {
         // Inicialización del componente
+    }
+
+    onSubmit(form: NgForm) {
+        // Lógica para manejar el envío del formulario
+        console.log('Formulario enviado');
+    }
+
+    limpiarFormulario(form: NgForm) {
+        // Lógica para limpiar el formulario
+        form.resetForm();
+        this.logBookFilter = {};
+        console.log('Formulario limpiado');
     }
 
 }
--- src/app/modules/logbook/component/logbook-table/table.component.ts
+++ src/app/modules/logbook/component/logbook-table/table.component.ts
@@ -1,17 +1,60 @@
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
     selector: 'app-logbook-table',
     templateUrl: './table.component.html',
     styleUrls: ['./table.component.scss'],
+    imports: [MatTableModule, MatPaginatorModule, MatButtonModule, CommonModule, MatIconModule],
+
     standalone: true
 })
 export class LogbookTableComponent implements OnInit {
 
-    constructor() { }
+    loading: boolean = false;
+    totalItems: number = 0;
+    pageSize: number = 10;
+    pageIndex: number = 0;
+    pageSizeOptions: number[] = [5, 10, 20];
+    dataSource = [{
+        date: new Date(),
+        userName: 'John Doe',
+        description: 'Sample description',
+        actionType: 'CREATE',
+        module: 'User Management'
+    },
+    {
+        date: new Date(),
+        userName: 'Jane Smith',
+        description: 'Another sample description',
+        actionType: 'UPDATE',
+        module: 'Order Management'
+    }];
 
-    ngOnInit(): void {
-        // Inicialización del componente
+    displayedColumns: string[] = [
+        'date',
+        'userName',
+        'actionType',
+        'module',
+        'description',
+    ];
+
+    ngOnInit() {
     }
 
+    async getDataSource() {
+    }
+
+    editAlert(element: any) {
+    }
+
+    onPageChange(event: any) {
+        this.pageIndex = event.pageIndex === 0 ? 1 : event.pageIndex + 1;
+        this.pageSize = event.pageSize;
+        this.getDataSource();
+    }
 }
