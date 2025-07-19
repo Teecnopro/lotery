4209ab58c29f21e90f9e1abe38dc78a88d4927e7
MonkeyStriker <jesusdeveloper404@gmail.com>
2025-07-18 19:17:51 -0500

    Ajustes de total faltantes

--- src/app/infrastructure/delete-bets/delete-bets.adapter.ts
+++ src/app/infrastructure/delete-bets/delete-bets.adapter.ts
@@ -40,7 +40,7 @@ export class FirebaseDeleteBetsAdapter implements DeleteBetsServicePort {
   private betsSubject: BehaviorSubject<ListDeleteBets | null> =
     new BehaviorSubject<ListDeleteBets | null>(null);
   deleteProgress = new BehaviorSubject<number>(0);
-  private batchSize = 1;
+  private batchSize = 500;
 
   constructor(private firestore: Firestore) {}
 
