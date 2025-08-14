e72033b7708c99cd54e4a7f9826f1dd69730a7a8
MonkeyStriker <jesusdeveloper404@gmail.com>
2025-08-13 19:01:08 -0500

    Ajuste url api

--- environment/environment.ts
+++ environment/environment.ts
@@ -9,5 +9,5 @@ export const environment = {
     messagingSenderId: '161152013906',
     measurementId: 'G-5WRTMHSWXQ',
   },
-  api: "http://localhost:3000/",
+  api: "https://lotery-95y5kgqa0-carloslopez-1546s-projects.vercel.app/",
 };
