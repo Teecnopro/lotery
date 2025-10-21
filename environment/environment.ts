a557ef7d12ee0257ae747649fd521dbeffbb07ec
Carl0sL0pez03 <clopez5@udi.edu.co>
2025-10-21 09:16:34 -0500

    Update credentials.

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    projectId: 'lotery-c9221',
-    appId: '1:161152013906:web:f88a5eb6614801a232b18b',
-    storageBucket: 'lotery-c9221.firebasestorage.app',
-    apiKey: 'AIzaSyAWC9dzYI0sbsPQoSEZ37cHZ7ZbEwOWOas',
-    authDomain: 'lotery-c9221.firebaseapp.com',
-    messagingSenderId: '161152013906',
-    measurementId: 'G-5WRTMHSWXQ',
+    apiKey: 'AIzaSyBe7elJ_VTblzoepqMv5RL45wC2ShWeNJY',
+    authDomain: 'lotery-a6e1e.firebaseapp.com',
+    projectId: 'lotery-a6e1e',
+    storageBucket: 'lotery-a6e1e.firebasestorage.app',
+    messagingSenderId: '428315639029',
+    appId: '1:428315639029:web:2d04380f2c5172aa70d9bc',
+    measurementId: 'G-G8HZJW2TQQ',
   },
   api: "https://lotery-api.vercel.app/",
 };
